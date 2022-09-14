1201ed317b17671fcd8375fb48f7adcd44258a02
Ngoni Seremwe <seremwen@gmail.com>
2022-09-14 12:10:23 +0200

    refactor

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=monolithadmin
+export POSTGRES_PASSWORD=monolith123
+export POSTGRES_HOST=monolithdb.cmg1opi9nw4c.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=arn:aws:s3:::project-monolith
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
